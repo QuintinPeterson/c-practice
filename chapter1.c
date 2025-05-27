f92b390abd5e39dda3ea64b4bf1b954d78c94702
Quintin <quintinp311204@gmail.com>
2025-05-27 13:40:00 -0500

    update progress

--- chapter1.c
+++ chapter1.c
@@ -5,15 +5,30 @@ int power(int m, int n);
 
 float fahrCelsius(float fahr);
 
+int get_line(char line[], int maxline);
+
+void copy(char to[], char from[]);
+
+#define MAXLINE 1000 /* maximum input line size */
+
 
 int main() { 
 
+    /* print longest input line */
+    int len;                /* current line length */
+    int max;                /* maximum length seen so far */
+    char line[MAXLINE];     /* current input line */
+    char longest[MAXLINE];  /* longest line saved here */
 
-
-
-
-
-
+    max = 0;
+    while ((len = get_line(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0)    /* there was a line */
+        printf("%s", longest);
+    
 /* | Fahr-celsius conversion table using a function |
     float fahr;
     float celsius;
@@ -36,6 +51,33 @@ int main() {
         printf("%d %d %d \n", i, power(2,1), power(-3,i));
 */
     return 0;    
+}
+
+/* getline: read a line into s, return length */
+int get_line(char s[], int lim) 
+{
+    int c;
+    int i;
+
+    for (i=0; i<lim-1 && (c=getchar()) != EOF && c!= '\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
 }
 
 float fahrCelsius(float fahr){
