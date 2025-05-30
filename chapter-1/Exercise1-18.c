625b148ceb1b50aeffd5a803afc82d28ea586a33
Quintin <quintinp311204@gmail.com>
2025-05-29 20:38:05 -0500

    update progress

---
+++ chapter-1/Exercise1-18.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int get_line(char line[], int maxline);
+
+void copy(char to[], char from[]);
+
+#define MAXLINE 1000 /* maximum input line size */
+
+
+int main() { 
+
+    char line[MAXLINE];     /* current input line */
+    char verified[MAXLINE];
+
+    get_line(line, MAXLINE);
+    copy(verified, line);
+
+    
+    
+
+    return 0;    
+}
+
+/* getline: read a line into s, return length */
+int get_line(char s[], int lim) 
+{
+    int c;
+    int i;
+
+    for (i=0; i<lim-1 && (c=getchar()) != EOF && c!= '\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
