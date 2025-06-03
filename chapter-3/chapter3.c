551f943a0fcfadc1b471a9c17bb353debe0841a4
vuintin <quintinp311204@gmail.com>
2025-06-03 15:35:58 -0500

    updated progress

---
+++ chapter-3/chapter3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n);
+
+int main()
+{
+
+    return 0;
+}
+
+/* binsrearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+int binsearch(int x, int v[], int n)
+{
+    int low;
+    int high;
+    int mid;
+
+    low = 0;
+    high = n-1;
+    while (low <= high) {
+        mid = (low+high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else /* found match */
+            return mid;
+    }
+    return -1; /* no match */
+
+}
