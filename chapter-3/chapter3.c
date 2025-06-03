7ccaff6e1a196b071690ff6aa4746e096acdbe6a
vuintin <quintinp311204@gmail.com>
2025-06-03 17:57:25 -0500

    updated progress

--- chapter-3/chapter3.c
+++ chapter-3/chapter3.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int binsearch(int x, int v[], int n);
+
+int atoi(char s[]);
+
+void shellsort(int v[], int n);
 
 int main()
 {
@@ -31,6 +36,37 @@ int main()
         nwhite, nother);
 
     return 0;
+}
+
+/* shellsort: sort v[0]...v[n-1] into incrementing order */
+void shellsort(int v[], int n)
+{
+    int gap, i, j, temp;
+
+    for (gap = n/2; gap > 0; gap /= 2)
+        for (i = gap; i < n; i++)
+            for (j=i-gap; j>=0 && v[j]>v[j+gap]; j-=gap) {
+                temp = v[j];
+                v[j] = v[j+gap];
+                v[j+gap] = temp;
+            }
+
+}
+
+/* atoi: convert s to integer; version 2 */
+int atoi(char s[])
+{
+    int i, n, sign;
+
+    for (i = 0; isspace(s[i]); i++) /* skips whitespace */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-') /* skip sign */
+        i++;
+    for (n = 0; isdigit(s[i]); i++)
+        n = 10 * n + (s[i] - '0');
+    return sign * n;
+
 }
 
 /* binsrearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
