81f1cc553705df12aac4996c98ebb7c157fc5100
vuintin <quintinp311204@gmail.com>
2025-06-04 20:33:05 -0500

    finished exercise4-1

--- chapter-4/Exercise4-1.c
+++ chapter-4/Exercise4-1.c
@@ -2,7 +2,7 @@
 #define MAXLINE 1000    /* maximum input line length */
 
 int getline(char line[], int max);
-int strindex(char source[], char searchfor[]);
+int strrindex(char source[], char searchfor[]);
 
 char pattern[] = "ould";    /* pattern to search for */
 
@@ -13,7 +13,7 @@ int main()
     int found = 0;
 
     while (getline(line, MAXLINE) > 0)
-        if (strindex(line, pattern) >= 0) {
+        if (strrindex(line, pattern) >= 0) {
             printf("%s", line);
             found++;
         }
@@ -36,15 +36,16 @@ int getline(char s[], int lim)
 }
 
 /* strindex: return index of t in s, -1 if none */
-int strindex(char s[], char t[])
+int strrindex(char s[], char t[])
 {
     int i, j, k;
+    int pos = -1;
 
     for (i = 0; s[i] != '\0'; i++) {
         for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
             ;
         if (k > 0 && t[k] == '\0')
-            return i;
+            pos = i;
     }
-    return -1;
+    return pos;
 }
