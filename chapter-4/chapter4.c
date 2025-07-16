3066832c81a1d575fa3e4f1de4c710fc26a5cdf2
vuintin <quintinp311204@gmail.com>
2025-07-16 15:52:36 -0500

    update progress

--- chapter-4/chapter4.c
+++ chapter-4/chapter4.c
@@ -6,6 +6,7 @@
 #define MAXOP 100   /* max size of operand or operator */
 #define NUMBER '0'  /* signal that a number was found */
 #define MAXVAL 100 /* maximum depth of val stack */
+#define BUFSIZE 100
 
 int getop(char []);
 void push(double);
@@ -80,6 +81,46 @@ double pop(void)
         printf("error: stack empty\n");
         return 0.0;
     }
+}
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+    int i;
+    int c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1]='\0';
+    if (!isdigit(c) && c != '.')
+        return c;   /* not a number */
+    i = 0;
+    if (isdigit(c)) /* collect integer part*/
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+char buf[BUFSIZE];  /* buffer for ungetch */
+int bufp = 0;   /* next free position in buf */
+
+int getch(void) /* get a (possibly pushed back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* push character back on input */
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
 }
 
 /* atoi: convert string s to integer using atof 
